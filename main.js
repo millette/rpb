38539c9dfbe1e30918b9263215e26bf0d7d11ff7
Robin Millette <robin@millette.info>
2019-05-19 16:58:00 -0400

    es5 modulation.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // npm
-const PouchDB = require('pouchdb-core')
-  .plugin(require('pouchdb-adapter-memory'))
+import PouchDB from 'pouchdb-core'
+import memory from 'pouchdb-adapter-memory'
+
+PouchDB.plugin(memory)
 
 const db = new PouchDB('mydb', {adapter: 'memory'})
 console.log(db.adapter) // 'memory'

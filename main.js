fd001de06617a868901ac637d147710b5304c4e4
Robin Millette <robin@millette.info>
2019-05-19 22:35:24 -0400

    Display data with export options.

--- main.js
+++ main.js
@@ -5,29 +5,82 @@ import memory from 'pouchdb-adapter-memory'
 PouchDB.plugin(memory)
 
 const db = new PouchDB('mydb', {adapter: 'memory'})
-console.log(db.adapter) // 'memory'
 
+// dom utils
 const mainEl = document.querySelector('main')
+const $el = (tag, attr = {}) => {
+  const el = document.createElement(tag)
+  let r
+  for (r in attr) el[r] = attr[r]
+  return el
+}
 
-fetch('/initial-batch.json')
+const $add = (p, c) => p.appendChild(c) && p
+const appendToMain = (c) => $add(mainEl, c)
+
+// pouchdb utils
+const all = () => db.allDocs({ include_docs: true })
+
+const makeDetails = (doc) => $el('pre', { innerText: JSON.stringify(doc, null, '  ') })
+
+const domDoc = ({ doc }) => $add(
+  $el('li'),
+  $add(
+    $add($el('details'), makeDetails(doc)),
+    $el('summary', { innerText: doc.name || doc.title || doc._id })
+  )
+)
+
+const download = function (withRevs, { rows }) {
+  const mdoc = withRevs ? (({doc}) => doc) : (({doc}) => ({ ...doc, _rev: undefined }))
+  const blob = new Blob([JSON.stringify(rows.map(mdoc))], { type: "application/json" })
+  this.href = URL.createObjectURL(blob)
+  this.click()
+}
+
+const makeDownload = (withRev) => $el('a', {
+  href: '#',
+  target: '_blank',
+  innerText: `Export with${withRev ? '' : 'out'} revs`,
+  onclick: function (ev) {
+    if (!this.pathname) return
+    ev.preventDefault()
+    all().then(download.bind(this, withRev))
+  }
+})
+
+const makeDomDocs = ({ rows }) => {
+  const ulEl = $el('ul')
+  const li = (el) => $add(ulEl, el)
+  rows.map(domDoc).forEach(li)
+  return ulEl
+}
+
+const makeNav = () => {
+  const nav = $el('ul')
+  const navA = $el('li')
+  const navB = $el('li')
+  $add(nav, $add(navA, makeDownload(false)))
+  $add(nav, $add(navB, makeDownload(true)))
+  return nav
+}
+
+const domStuff = (rows) => {
+  appendToMain(makeDomDocs(rows))
+  appendToMain(makeNav())
+  return 'All DOM!'
+}
+
+const init = () => fetch('/initial-batch.json')
   .then((res) => res.json())
   .then(db.bulkDocs)
-  .then((json) => {
-    const ulEl = document.createElement('ul')
-    mainEl.appendChild(ulEl)
-    console.log('JSON-0', json)
-    const [{ id }] = json
+  .then(all)
+  .then(domStuff)
 
-    const liEl = document.createElement('li')
-    liEl.innerText = id
-    ulEl.appendChild(liEl)
-
-    return Promise.all([liEl, db.get(id)])
+init()
+  .then((x) => {
+    console.log(x)
   })
-  .then(([liEl, json]) => {
-    console.log('JSON-1', json)
-    const preEl = document.createElement('pre')
-    preEl.innerText = JSON.stringify(json, null, '  ')
-    liEl.innerText = ''
-    liEl.appendChild(preEl)
+  .catch((e) => {
+    console.error(e)
   })

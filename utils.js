16323819e63f21d81ff8c2ed8a66a6ac342a47c0
Robin Millette <robin@millette.info>
2019-05-21 06:51:17 -0400

    Prettier utils.

--- utils.js
+++ utils.js
@@ -5,15 +5,17 @@ import { setMessage } from "./dom.js"
 import { all, domStuff } from "./misc.js"
 
 // globals
-const DBNAME = 'oy' //  // mydb
+const DBNAME = "oy" //  // mydb
 const DB = new PouchDB(DBNAME)
 
 // console.log('DB-keys', DB.name, Object.keys(DB))
 
-const init = () => window.fetch('/initial-batch.json')
-  .then((res) => res.json())
-  .then(DB.bulkDocs)
-  .then(all.bind(this, DB))
-  .then(domStuff.bind(this, DB))
+const init = () =>
+  window
+    .fetch("/initial-batch.json")
+    .then((res) => res.json())
+    .then(DB.bulkDocs)
+    .then(all.bind(this, DB))
+    .then(domStuff.bind(this, DB))
 
 export { init, setMessage }

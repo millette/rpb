8e2ea6fec1d5427928d3c06aa8fe461cf1b23d58
Robin Millette <robin@millette.info>
2019-05-21 06:42:45 -0400

    Extract dom utils.

---
+++ dom.js
@@ -0,0 +1,30 @@
+// dom utils
+const mainEl = document.querySelector('main')
+
+const setMessage = (() => {
+  const errorEl = mainEl.querySelector('#error-message')
+  let errorTimer
+
+  return (msg, ms = 5000) => {
+    errorEl.innerText = msg || ''
+    if (msg && ms) {
+      clearTimeout(errorTimer)
+      errorTimer = setTimeout(setMessage, ms)
+    }
+  }
+})()
+
+const $el = (tag, attr) => {
+  const el = document.createElement(tag)
+  if (!attr) return el
+  if (typeof attr === 'string') attr = { innerText: attr }
+  if (typeof attr !== 'object') throw new Error('Expected object or string (for .innerText).')
+  let r
+  for (r in attr) el[r] = attr[r]
+  return el
+}
+
+const $add = (p, c) => p.appendChild(c) && p
+const appendToMain = (c) => $add(mainEl, c)
+
+export { setMessage, $el, $add, appendToMain }

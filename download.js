438211fab99b91740b487e39aa9ac026967b1d76
Robin Millette <robin@millette.info>
2019-05-21 06:44:00 -0400

    Extract download utils.

---
+++ download.js
@@ -0,0 +1,22 @@
+import { $el } from "./dom.js"
+import { all } from "./misc.js"
+
+const download = function (withRevs, { rows }) {
+  const mdoc = withRevs ? (({doc}) => doc) : (({doc}) => ({ ...doc, _rev: undefined }))
+  const blob = new window.Blob([JSON.stringify(rows.map(mdoc))], { type: "application/json" })
+  this.href = URL.createObjectURL(blob)
+  this.click()
+}
+
+const makeDownload = (DB, withRev) => $el('a', {
+  href: '#',
+  target: '_blank',
+  innerText: `Export with${withRev ? '' : 'out'} revs`,
+  onclick: function (ev) {
+    if (!this.pathname) return
+    ev.preventDefault()
+    all(DB).then(download.bind(this, withRev))
+  }
+})
+
+export default makeDownload

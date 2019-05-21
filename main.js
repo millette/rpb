c139e99aa10e40dc792a49deeb42ca501508d11c
Robin Millette <robin@millette.info>
2019-05-20 22:57:40 -0400

    Open edit on new doc.

--- main.js
+++ main.js
@@ -4,12 +4,16 @@ import memory from 'pouchdb-adapter-memory'
 
 PouchDB.plugin(memory)
 
-const db = new PouchDB('mydb', {adapter: 'memory'})
+const UI_TIMEOUT = 50
+const db = new PouchDB('mydb', { adapter: 'memory' })
 
 // dom utils
 const mainEl = document.querySelector('main')
-const $el = (tag, attr = {}) => {
+const $el = (tag, attr) => {
   const el = document.createElement(tag)
+  if (!attr) return el
+  if (typeof attr === 'string') attr = { innerText: attr }
+  if (typeof attr !== 'object') throw new Error('Expected object or string (for .innerText).')
   let r
   for (r in attr) el[r] = attr[r]
   return el
@@ -21,15 +25,94 @@ const appendToMain = (c) => $add(mainEl, c)
 // pouchdb utils
 const all = () => db.allDocs({ include_docs: true })
 
-const makeDetails = (doc) => $el('pre', { innerText: JSON.stringify(doc, null, '  ') })
+const asEdit = (pre, doc) => {
+  const ta = $el('textarea', { rows: 10, innerHTML: JSON.stringify(doc, null, '  ') })
+  ta.onkeydown = (ev) => {
+    if (!(ev.ctrlKey && (ev.code === 'Enter'))) return
+    const nDoc = JSON.parse(ev.target.value)
+    if (nDoc._rev !== doc._rev) throw new Error('Do not modify the _rev field manually.')
 
-const domDoc = ({ doc }) => $add(
-  $el('li'),
-  $add(
-    $add($el('details'), makeDetails(doc)),
-    $el('summary', { innerText: doc.name || doc.title || doc._id })
+    let willDelete
+    const method = nDoc._id ? 'put' : 'post'
+
+    if (doc._id && (nDoc._id !== doc._id)) {
+      const buzz = confirm('Are you sure you want to create a new document by changing its id field?')
+      if (!buzz) throw new Error('Cancelled after id field change.')
+      const buzz2 = confirm('Also remove the original document?')
+      delete nDoc._rev
+      willDelete = buzz2
+    }
+    const p = [db[method](nDoc)]
+    if (willDelete) p.push(db.remove(doc))
+    Promise.all(p)
+      .then(([p0, p1]) => {
+        if (!p0.ok) throw new Error('Put/Post error.')
+
+        doc = {
+          ...nDoc,
+          _id: p0.id,
+          _rev: p0.rev
+        }
+        pre.innerText = JSON.stringify(doc, null, '  ')
+        document.querySelectorAll('#exports > li > a')
+          .forEach((el) => {
+            const { pathname, href } = el
+            if (pathname) return
+            el.href = '#'
+            URL.revokeObjectURL(href)
+          })
+        if (p1 && !p1.ok) throw new Error('Put/Post error.')
+      })
+  }
+  pre.innerText = ''
+  $add(pre, ta)
+  setTimeout(() => ta.focus(), UI_TIMEOUT)
+}
+
+const asRead = (pre, doc) => {
+  pre.innerText = JSON.stringify(doc, null, '  ')
+}
+
+const makeDetails = (doc) => {
+  const divEl = $el('div')
+  const preEl = $el('pre', JSON.stringify(doc, null, '  '))
+  $add(divEl, preEl)
+  if (!Object.keys(doc).length) asEdit(preEl, {})
+  const buttonsEl = $el('div')
+
+  $add(buttonsEl, $el('button', {
+    innerText: 'show',
+    onclick: (ev) => {
+      ev.preventDefault()
+      asRead(preEl, doc)
+    }
+  }))
+
+  $add(buttonsEl, $el('button', {
+    innerText: 'edit',
+    onclick: (ev) => {
+      ev.preventDefault()
+      asEdit(preEl, doc)
+    }
+  }))
+
+  $add(buttonsEl, $el('button', 'b2'))
+  $add(buttonsEl, $el('button', 'b3'))
+  return $add(divEl, buttonsEl)
+}
+
+const domDoc = ({ doc }) => {
+  const ed = !Object.keys(doc).length
+  const summary = $el('summary', ed ? 'NEW-DOC' : (doc.name || doc.title || doc._id || 'NEW-DOC?'))
+  if (ed) setTimeout(() => summary.click(), UI_TIMEOUT)
+  return $add(
+    $el('li', { 'data-key': doc._id }),
+    $add(
+      $add($el('details'), makeDetails(doc)),
+      summary
+    )
   )
-)
+}
 
 const download = function (withRevs, { rows }) {
   const mdoc = withRevs ? (({doc}) => doc) : (({doc}) => ({ ...doc, _rev: undefined }))
@@ -50,19 +133,47 @@ const makeDownload = (withRev) => $el('a', {
 })
 
 const makeDomDocs = ({ rows }) => {
-  const ulEl = $el('ul')
+  const gg = document.querySelector('#shown-docs')
+  const ulEl = gg || $el('ul', { id: 'shown-docs' })
+  if (gg) ulEl.innerText = ''
   const li = (el) => $add(ulEl, el)
   rows.map(domDoc).forEach(li)
   return ulEl
 }
 
 const makeNav = () => {
-  const nav = $el('ul')
+  const div = $el('div')
+
+  const more = $el('p')
+  const but = $el('button', {
+    innerText: 'New Doc',
+    onclick: (ev) => {
+      ev.preventDefault()
+      all()
+        .then(({ rows }) => ({ rows: [{ doc: {  } }, ...rows] }))
+        .then(makeDomDocs)
+    }
+  })
+
+  const but2 = $el('button', {
+    innerText: 'Refresh',
+    onclick: (ev) => {
+      ev.preventDefault()
+      all().then(makeDomDocs)
+    }
+  })
+
+  $add(more, but)
+  $add(more, but2)
+
+  const nav = $el('ul', { id: 'exports' })
   const navA = $el('li')
   const navB = $el('li')
   $add(nav, $add(navA, makeDownload(false)))
   $add(nav, $add(navB, makeDownload(true)))
-  return nav
+  $add(div, more)
+  $add(div, nav)
+  return div
 }
 
 const domStuff = (rows) => {

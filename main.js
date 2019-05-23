7de2f13eefab084de53758c2062ba775ac8a082c
Robin Millette <robin@millette.info>
2019-05-23 01:52:34 -0400

    Fix main.

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 // self
 import { init, setMessage, defaultDBName } from "./lib/utils.js"
 
-const where = new URL(window.location.href).searchParams.get("db-name")
-
-if (where === defaultDBName) return (window.location.href = ".")
-
-init()
-  .then(setMessage)
-  .catch(setMessage)
+if (
+  new URL(window.location.href).searchParams.get("db-name") === defaultDBName
+) {
+  window.location.href = "."
+} else {
+  init()
+    .then(setMessage)
+    .catch(setMessage)
+}

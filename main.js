7e95ab4699e796b7731e46d83d65e4cba42d384a
Robin Millette <robin@millette.info>
2019-05-23 01:44:51 -0400

    Redirect to front early.

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 // self
-import { init, setMessage } from "./lib/utils.js"
+import { init, setMessage, defaultDBName } from "./lib/utils.js"
+
+const where = new URL(window.location.href).searchParams.get("db-name")
+
+if (where === defaultDBName) {
+  window.location.href = "."
+  return
+}
 
 init()
   .then(setMessage)
